b249a36b4454217434a5e1485554962560d38a28
romuloFranc0 <rmaciel@id.uff.br>
2025-03-29 09:50:02 -0300

    adding securityfilter before security filter chain, to validate token

--- src/main/java/romulofranc0/movie_tracker/infra/security/config/SecurityConfig.java
+++ src/main/java/romulofranc0/movie_tracker/infra/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package romulofranc0.movie_tracker.infra.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import romulofranc0.movie_tracker.infra.security.SecurityFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -27,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST,"/review").hasRole("USER")
                         .anyRequest().authenticated())
-
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
