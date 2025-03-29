fd9f4e13964e0221cc3661df19ece97bace4ba40
romuloFranc0 <rmaciel@id.uff.br>
2025-03-29 09:29:20 -0300

    JWT token create and verify

---
+++ src/main/java/romulofranc0/movie_tracker/infra/security/services/TokenService.java
@@ -0,0 +1,50 @@
+package romulofranc0.movie_tracker.infra.security.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.bouncycastle.math.ec.rfc8032.Ed25519;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(UserDetails userDetails) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("movie-track")
+                    .withSubject(userDetails.getUsername())
+                    .withExpiresAt(generateExpirationTime())
+                    .sign(algorithm);
+        }catch(JWTCreationException exception){
+            throw new RuntimeException("Error generating token",exception);
+
+        }
+    }
+    public String validateToken(String token) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("movie-track")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            return "";
+        }
+    }
+
+    private Instant generateExpirationTime() {
+        return Instant.now().plusSeconds(7200);
+    }
+}
