744cbfebc912f912d68eed80b4c2d5f8f23005c1
romuloFranc0 <rmaciel@id.uff.br>
2025-03-29 09:13:12 -0300

    constructor

--- src/main/java/romulofranc0/movie_tracker/domain/entities/AppUser.java
+++ src/main/java/romulofranc0/movie_tracker/domain/entities/AppUser.java
@@ -2,15 +2,16 @@ package romulofranc0.movie_tracker.domain.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 @Entity
 @Data
+@NoArgsConstructor
 public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +22,13 @@ public class AppUser implements UserDetails {
     @OneToMany(mappedBy = "user")
     private List<Review> reviews;
     private Role role;
+
+    public AppUser(String username, String password,String email, Role role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
