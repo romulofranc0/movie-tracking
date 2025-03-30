49f36adb4c50be27df71c0b37c6c95b498090961
romuloFranc0 <rfsvm97@gmail.com>
2025-03-30 08:40:21 -0300

    fixing token validation

--- src/main/java/romulofranc0/movie_tracker/infra/security/SecurityFilter.java
+++ src/main/java/romulofranc0/movie_tracker/infra/security/SecurityFilter.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import romulofranc0.movie_tracker.domain.entities.AppUser;
 import romulofranc0.movie_tracker.infra.repositories.UserRepository;
 import romulofranc0.movie_tracker.infra.security.services.TokenService;
 
@@ -28,7 +30,7 @@ public class SecurityFilter extends OncePerRequestFilter {
         var token = this.recoverToken(request);
         if (token != null) {
             var username = tokenService.validateToken(token);
-            UserDetails userDetails = userRepository.findByUsername(username);
+            AppUser userDetails = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
 
             var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -39,7 +41,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String recoverToken(HttpServletRequest request) {
         var header = request.getHeader("Authorization");
         if (header == null || !header.startsWith("Bearer "))return null;
-        return header.replace("Bearer","");
+        return header.substring(7).trim();
     }
 
 }
