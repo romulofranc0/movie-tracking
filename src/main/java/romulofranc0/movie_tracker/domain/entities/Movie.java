1bae18001b3d99f34283c0d1ca12a2b737a0f169
romulofranc0 <rfsvm97@gmail.com>
2025-07-17 22:23:27 -0300

    creating `MovieList` and mapping relationships

--- src/main/java/romulofranc0/movie_tracker/domain/entities/Movie.java
+++ src/main/java/romulofranc0/movie_tracker/domain/entities/Movie.java
@@ -1,11 +1,17 @@
 package romulofranc0.movie_tracker.domain.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
 public class Movie{
+
     @Id
     private String imdbID;
     private String title;
@@ -14,5 +20,8 @@ public class Movie{
     private String genre;
     private String poster;
     private String plot;
+
+    @ManyToMany(mappedBy = "movies")
+    private Set<MovieList> movieLists = new HashSet<MovieList>();
 
 }
