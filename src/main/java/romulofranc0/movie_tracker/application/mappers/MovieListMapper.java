01d4e0e5acce8f23a8ce33c99d41ea5b88d58b80
romulofranc0 <rfsvm97@gmail.com>
2025-07-17 22:12:18 -0300

    mappers

---
+++ src/main/java/romulofranc0/movie_tracker/application/mappers/MovieListMapper.java
@@ -0,0 +1,31 @@
+package romulofranc0.movie_tracker.application.mappers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import romulofranc0.movie_tracker.application.models.responses.MovieListResponse;
+import romulofranc0.movie_tracker.domain.entities.Movie;
+import romulofranc0.movie_tracker.domain.entities.MovieList;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Component
+public class MovieListMapper {
+
+    private final MovieMapper movieMapper;
+
+    public Set<MovieListResponse> toMovieListResponses(List<MovieList> movieLists) {
+        var movieListResponses= new HashSet<MovieListResponse>();
+        for (MovieList movieList : movieLists) {
+            var movieListResponse = new MovieListResponse(
+                    movieList.getId(),
+                    movieList.getListName(),
+                    movieMapper.movieToResponses(movieList.getMovies())
+            );
+            movieListResponses.add(movieListResponse);
+        }
+        return movieListResponses;
+    }
+}
---
+++ src/main/java/romulofranc0/movie_tracker/application/mappers/MovieMapper.java
@@ -0,0 +1,43 @@
+package romulofranc0.movie_tracker.application.mappers;
+
+import org.springframework.stereotype.Component;
+import romulofranc0.movie_tracker.application.models.responses.MovieResponse;
+import romulofranc0.movie_tracker.domain.entities.Movie;
+import romulofranc0.movie_tracker.domain.entities.MovieList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class MovieMapper {
+
+    public Set<MovieResponse> movieToResponses(Set<Movie> movies){
+        Set<MovieResponse> movieResponses= new HashSet<MovieResponse>();
+        for (Movie movie : movies) {
+            MovieResponse movieResponse = new MovieResponse(
+                    movie.getTitle(),
+                    movie.getDirector(),
+                    movie.getPlot(),
+                    movie.getYear(),
+                    movie.getGenre(),
+                    movie.getImdbID(),
+                    movie.getPoster()
+            );
+
+            movieResponses.add(movieResponse);
+        }
+        return movieResponses;
+    }
+
+    public MovieResponse toMovieResponse(Movie movie) {
+        return new MovieResponse(
+                movie.getTitle(),
+                movie.getDirector(),
+                movie.getPlot(),
+                movie.getYear(),
+                movie.getGenre(),
+                movie.getImdbID(),
+                movie.getPoster()
+        );
+    }
+}
