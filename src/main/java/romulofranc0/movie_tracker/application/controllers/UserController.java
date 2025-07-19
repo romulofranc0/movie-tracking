19a2d988f9508749cfd0721ea558b5270b60a9dd
romulofranc0 <rfsvm97@gmail.com>
2025-07-18 21:55:48 -0300

    creating controller and follow endpoint

---
+++ src/main/java/romulofranc0/movie_tracker/application/controllers/UserController.java
@@ -0,0 +1,24 @@
+package romulofranc0.movie_tracker.application.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import romulofranc0.movie_tracker.domain.services.UserService;
+import romulofranc0.movie_tracker.infra.repositories.UserRepository;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/follow/{id}")
+    public ResponseEntity<?> follow(@PathVariable Long id){
+        userService.follow(id);
+        return ResponseEntity.ok().build();
+    }
+}
