f606abb0736fd6c1890f84a32edc706a8debdabf
romulofranc0 <rfsvm97@gmail.com>
2025-09-05 12:21:43 -0300

    changing GenerationType.AUTO to IDENTITY

--- src/main/java/romulofranc0/movie_tracker/domain/entities/ReviewComment.java
+++ src/main/java/romulofranc0/movie_tracker/domain/entities/ReviewComment.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class ReviewComment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
    private String commentText;
